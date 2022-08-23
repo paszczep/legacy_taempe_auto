8ba196941f1f7833e6b95f2f489d73b19514ac13
pawel.szczepanski <pawel.szczepanski@algorytmik.io>
2022-08-23 23:00:11 +0200

    maybe works

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+import sched
 
 URL = 'https://gms.myreefer.com/ProActtransport/Logon'
 dotenv_file = dotenv.find_dotenv()
@@ -19,11 +20,13 @@ class Event:
         self.temperature = schedule_row['temperature']
 
 
-def get_schedule():
+def get_event_plan() -> list:
     file = [el for el in os.listdir() if '.csv' in el].pop()
     open_file = open(file, 'r')
     reader = DictReader(open_file)
     schedule = [Event(row) for row in reader]
+    for event in schedule:
+        print(event.time, event.container, f'{event.temperature} °C', sep='\t')
     return schedule
 
 
@@ -48,10 +51,10 @@ def sign_in(sign_in_driver: webdriver):
     fill_sign_in_field(sign_in_driver, 'Password', password)
     sign_in_button = sign_in_driver.find_element(By.CSS_SELECTOR, 'button.btn.btn-primary')
     sign_in_button.click()
-    time.sleep(10)
+    time.sleep(3)
 
 
-def set_temperature_on_container(container: str, temperature: str, setting_driver):
+def set_temperature(container: str, temperature: str, setting_driver):
     container_button = setting_driver.find_element(By.XPATH, f"//*[contains(text(), '{container}')]")
     container_button.click()
     menu_expand = setting_driver.find_element(By.CSS_SELECTOR, 'div.k-icon.k-collapse-prev')
@@ -65,31 +68,38 @@ def set_temperature_on_container(container: str, temperature: str, setting_drive
 
     change_temp_execute = executes[2]
     change_temp_execute.click()
-
+    time.sleep(2)
     temperature_input_field = setting_driver.find_element(By.XPATH, "//input[@placeholder='Set point']")
     temperature_input_field.send_keys(temperature)
+    #
+    # execute_button = setting_driver.find_element(By.CSS_SELECTOR, 'button.btn.btn-primary')
+    # execute_button.click()
 
-    execute_button = setting_driver.find_element(By.CSS_SELECTOR, 'button.btn.btn-primary')
-    execute_button.click()
+
+def create_event_schedule(events: list, driver: webdriver):
+    s = sched.scheduler(time.time, time.sleep)
+    for event in events:
+        kwargs = {'container': event.container, 'temperature': event.temperature, 'setting_driver': driver}
+        s.enterabs(event.time, 0, set_temperature, kwargs=kwargs)
+    s.run()
 
 
 def run_program():
-    plan = get_schedule()
-    click_contaner = plan[0].container
+    plan = get_event_plan()
+    click_container = plan[0].container
     temperature_to_set = plan[0].temperature
+    event_datetime = plan[0].time
 
-    print(click_contaner)
     driver = get_web_driver()
     driver.get(URL)
     driver.maximize_window()
     sign_in(driver)
 
-    set_temperature_on_container(
-        container=click_contaner,
+    set_temperature(
+        container=click_container,
         temperature=temperature_to_set,
         setting_driver=driver)
 
 
 if __name__ == '__main__':
-
     run_program()
